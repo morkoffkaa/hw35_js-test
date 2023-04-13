0725f23b6d0c8fda5bef30810f9738325f7967a5
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-04-13 18:01:35 +0300

    add js

--- pages/main.js
+++ pages/main.js
@@ -1,22 +1,32 @@
+document.getElementById("saveBtn").addEventListener("click", generateTable);
+
 function generateTable() {
-    let firstName = document.getElementById("first-name").value;
-    let lastName = document.getElementById("last-name").value;
-    let birthYear = document.getElementById("birth-year").value;
-    let sex = document.querySelector('input[name="sex"]:checked').value;
-    let city = document.getElementById("city").value;
-    let address = document.getElementById("address").value;
-
-
-    let languages = [];
-    let checkboxes = document.querySelectorAll('input[name="condition_type"]:checked').value;
-
-    for (let i = 0; i < checkboxes.length; i++) {
-        languages.push(checkboxes[i].value);
+ let firstName = document.getElementById("first-name").value;
+ let lastName = document.getElementById("last-name").value;
+ let birthYear = document.getElementById("birth-year").value;
+ let sex = document.querySelector('input[name="sex"]:checked').value;
+ let city = document.getElementById("city").value;
+ let address = document.getElementById("address").value;
+ let languages = [];
+ let languageCheckboxes = document.getElementsByName("language");
+  for (var i = 0; i < languageCheckboxes.length; i++) {
+    if (languageCheckboxes[i].checked) {
+      languages.push(languageCheckboxes[i].value);
     }
+  }
 
-    let tableData = "<table><tr><th>Ім`я</th><th>Прізвище</th><th>Рік народження</th><th>Стать</th><th>Місто</th><th>Адреса</th><th>Мови, якими володіє користувач</th>"
-    tableData += "<tr><td>" + firstName + "</td><td>" + lastName + "</td><td>" + birthYear + "</td><td>" + sex + "</td><td>" + city + "</td><td>" + address + "</td><td>" + languages.join(", ") + "</td></tr>";
-    tableData += "</table>";
+  
+  let table = document.createElement("table");
+  table.innerHTML = "<tr><td>Ім'я:</td><td>" + firstName + "</td></tr>" +
+    "<tr><td>Прізвище:</td><td>" + lastName + "</td></tr>" +
+    "<tr><td>Дата народження:</td><td>" + birthYear + "</td></tr>" +
+    "<tr><td>Стать:</td><td>" + sex + "</td></tr>" +
+    "<tr><td>Місто:</td><td>" + city + "</td></tr>" +
+    "<tr><td>Адреса:</td><td>" + address + "</td></tr>" +
+    "<tr><td>Мови, якими володіє:</td><td>" + languages.join(", ") + "</td></tr>";
 
-    document.getElementById("table-container").innerHTML = tableData;
+  
+  let tableContainer = document.getElementById("table-container");
+  tableContainer.innerHTML = "";
+  tableContainer.appendChild(table);
 }
